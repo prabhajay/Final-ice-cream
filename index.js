55e79e39613a91d0a84523729692c21d9cbc8c5d
Lakshmi <lakshmi.prbh@gmail.com>
2022-08-08 10:54:06 -0600

    expressJS inital done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-document.querySelector()
+const express=require("express");
+const app=express();
+const bodyparser=require("body-parser");
+const PORT=3000;
+app.use()
+//const port=3000;
+//app.get("/",function(request,response){
+//response.send("Hello");
+//});
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/contact.html');
+})
+//app.get('/about',(req,res)=>{
+  //  res.sendFile('about.html')
+//})
+app.get('/icecream',(req,res)=>{
+    res.sendFile(__dirname + 'icecream.html');
+})
+app.get('/order',(req,res)=>{
+    res.sendFile(__dirname + 'order.html');
+})
+app.get('/contact',(req,res)=>{
+    res.sendFile(__dirname + 'contact.html');
+})
+app.listen(PORT,()=>{
+    console.log('server started on port ${PORT}')
+})
+
+app.get('/contact',(req,res)=>{
+    res.send('contact.html')
+})
+//app.listen(3000,function()
+//{
+  //  console.log("server started on port 3000");
+//});
---
+++ index1.js
@@ -0,0 +1,6 @@
+document.querySelector(".submit").addEventListener("click",submit_button)
+
+function submit_button()
+{
+    alert("We've received your request and will call you call back within 48 hours.");
+}
